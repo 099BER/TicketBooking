1fd9d79e393b16bda0090429563874622da94138
099BER <18455766+099BER@users.noreply.github.com>
2021-12-20 13:24:30 +0800

    Initial

---
+++ TicketBookingServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TicketBookingServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
